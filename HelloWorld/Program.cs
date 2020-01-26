52e20eba60bf95421022b1b947255bc042703382
2019659 <MSC2019659@iit.ac.lk>
2020-01-26 15:04:17 +0530

    first commit

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Out.WriteLine("Hello World!");
+            String x, y = null;
+            x = Console.ReadLine();
+            Console.Out.WriteLine("Hello : " + x);
+            y = Console.ReadLine();
+        }
+    }
+}
